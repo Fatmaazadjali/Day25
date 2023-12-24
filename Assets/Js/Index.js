c6db237b4a5fae2a47407e767009f1b9a2e25f08
Fatmazadjali <maathermohammed77@gmail.com>
2023-12-24 14:59:03 +0400

    js

---
+++ Assets/Js/Index.js
@@ -0,0 +1,40 @@
+
+
+
+ var a= "world";
+ let a1 = "from js";
+ 
+
+ myInfo = {name: "Fatma", lastName: "AlZadjali", Age: 23 };
+
+console.log(`${a} ${myInfo}` );
+
+function myfun(x , y=2){
+ return 1 * 3;
+};
+
+
+let f = "uydhhhdgsb";
+
+let Add = (a, b) => a+b;
+console.log(myfun);
+console.log(Add(2,6));
+console.log(f.length)
+
+let isEven(x, y){
+x = 
+};
+
+alert("enter yor password"
+);
+
+for (let i =2; i <7; i++){
+    console.log(i);
+    console.log("*".repeat(i))
+}
+
+function newColor(){
+  const ch = document.getElementsByClassName("sec-1")  
+}
+
+
